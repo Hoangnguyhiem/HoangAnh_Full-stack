f077fc341e303b4d4640018d818e7efc1a2c4289
Hoangnguyhiem <vinsomatem97@gmail.com>
2024-08-17 23:31:27 +0700

    update1 17-8

--- be/vite.config.js
+++ be/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     // ...vite configures
     server: {
         // vite server configs, for details see [vite doc](https://vitejs.dev/config/#server-host)
+        host: true,
         port: 8080,
     },
     plugins: [
